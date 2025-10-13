d22110b69496d81e72c43c71e608ff6af0f58ae3
SKYRENZ <LISTANGCO21@GMAIL.COM>
2025-10-14 02:32:05 +0800

    Refactor map.js to use serverless-http wrapper
    Replaces direct export of handler with a serverless-http wrapped Express app for improved compatibility with Netlify serverless functions.

--- LearnQuake/netlify/functions/map.js
+++ LearnQuake/netlify/functions/map.js
@@ -1,4 +1,8 @@
+import serverless from 'serverless-http';
+import app, { handler as appHandler } from '../../backend/serverlessApp.js';
 
+export const handler = async (event, context) => {
+  const wrapped = serverless(app);
+  return wrapped(event, context);
+};
 
-
-export { handler } from '../../backend/serverlessApp.js';
