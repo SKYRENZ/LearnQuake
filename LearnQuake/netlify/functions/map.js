a30a12beaffd3638375efabd03971ab126c25ee4
SKYRENZ <LISTANGCO21@GMAIL.COM>
2025-10-14 02:30:52 +0800

    Remove unused imports in map.js
    Cleaned up map.js by removing unnecessary imports of serverless and appHandler, leaving only the export of handler from serverlessApp.js.

--- LearnQuake/netlify/functions/map.js
+++ LearnQuake/netlify/functions/map.js
@@ -1,4 +1,4 @@
-import serverless from 'serverless-http';
-import app, { handler as appHandler } from '../../backend/serverlessApp.js';
+
+
 
 export { handler } from '../../backend/serverlessApp.js';
