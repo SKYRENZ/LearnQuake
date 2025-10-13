b69b679187dc742c45db3b154beedcd786fc9c3c
SKYRENZ <LISTANGCO21@GMAIL.COM>
2025-10-14 02:35:21 +0800

    Update map.js

--- LearnQuake/netlify/functions/map.js
+++ LearnQuake/netlify/functions/map.js
@@ -1,9 +1,7 @@
 import serverless from 'serverless-http';
-import app, { handler as appHandler } from '../../backend/serverlessApp.js';
+import app from '../../backend/serverlessApp.js';
 
 export const handler = async (event, context) => {
   const wrapped = serverless(app);
   return wrapped(event, context);
 };
-
-export { appHandler };
