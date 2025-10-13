ce77bf718b0042e776a5a6814c9ce6f27a89a565
SKYRENZ <LISTANGCO21@GMAIL.COM>
2025-10-13 14:09:49 +0800

    Add Netlify route for map API
    Added support for the '/.netlify/functions/map' route to allow map API access when deployed on Netlify, in addition to the local '/map' route.

--- LearnQuake/backend/serverlessApp.js
+++ LearnQuake/backend/serverlessApp.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/earthquakes', earthquakeRoutes);
+// allow both local "/map" and Netlify "/.netlify/functions/map"
 app.use('/map', mapRoutes);
+app.use('/.netlify/functions/map', mapRoutes);
 
 export default app;
