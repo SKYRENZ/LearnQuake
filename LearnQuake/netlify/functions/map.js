693ef2ad5626a16d9d0abacf2374aa54f46d6384
SKYRENZ <LISTANGCO21@GMAIL.COM>
2025-10-14 02:22:55 +0800

    Refactor map function handler and update Netlify config
    Simplifies the map function by directly exporting the handler from serverlessApp.js. Updates netlify.toml to specify external node modules and bundler settings for the map function.

--- LearnQuake/netlify/functions/map.js
+++ LearnQuake/netlify/functions/map.js
@@ -1,7 +1,4 @@
 import serverless from 'serverless-http';
 import app, { handler as appHandler } from '../../backend/serverlessApp.js';
 
-export const handler = async (event, context) => {
-  const wrapped = serverless(app);
-  return wrapped(event, context);
-};
+export { handler } from '../../backend/serverlessApp.js';
